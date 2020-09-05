d308b8e41c04cf61081d0bf7b131517f98b67492
RokwonK <wonrok97@naver.com>
2020-09-05 21:29:54 +0900

    2020/09/05

---
+++ models/lolcomment.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+// users 테이블 : posts 테이블 = 1:N
+module.exports = class LoLComment extends Model {
+    static initialize(sequelize) {
+        return super.init(
+            {
+                
+            },
+            {
+                sequelize,
+                modelName : 'LoLComment',
+                tableName : 'lolcomments',
+
+                timestamps : true,
+                // deletedAt 생성 => 데이터복구를 연동
+                // (NULL이면 삭제 안된거, 데이터 조회시 NULL인것만 조회됨)
+                paranoid : true,
+                // column명을 create_at이 아닌 createAt으로 표시
+                underscored : false,
+                charset : 'utf8'
+            }
+        )
+    }
+    static associate(db) {
+        
+    }
+}
